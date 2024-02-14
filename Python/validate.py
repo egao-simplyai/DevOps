97c3dc3287f405582bb3faf2a08a68eb5da236de
emily gao <egao@simplyai.com.au>
2024-02-15 10:33:21 +1100

    First Commit

---
+++ Python/validate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import snowflake.connector
+
+# Gets the version
+ctx = snowflake.connector.connect(
+    user='',
+    password='',
+    #account='ct46093.ap-southeast-2s.aws'
+    #account='eieookv.bs67383.ap-southeast-2s'
+    #https://ct46093.ap-southeast-2.snowflakecomputing.com
+    #EIEOOKV.BS67383
+    account='ORGNAME.ACCOUNTNAME.ap-southeast-2s'
+    )
+cs = ctx.cursor()
+try:
+    cs.execute("SELECT current_version()")
+    one_row = cs.fetchone()
+    print(one_row[0])
+finally:
+    cs.close()
+ctx.close()
